8536ecd9791366249f613fe3203da0057b52c9aa
bisubus <bisubus.gh@gmail.com>
2024-07-29 11:56:15 +0300

    Update init script

--- scripts/init.js
+++ scripts/init.js
@@ -3,7 +3,8 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as url from 'node:url';
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const args = process.argv.slice(2);
 const params = new Map(
@@ -22,6 +23,7 @@ const packageName = params.get('package');
 assert.ok(packageName, 'package');
 assert.ok(authorName, 'author');
 
+// Update package.json similarly to `npm pkg set`
 const packageJsonPath = path.join(__dirname, '..', 'package.json');
 const packageJsonData = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
@@ -35,6 +37,7 @@ fs.writeFileSync(
   'utf8',
 );
 
+// Update license
 const licensePath = path.join(__dirname, '..', 'LICENSE');
 let licenseData = fs.readFileSync(licensePath, 'utf8');
 const year = new Date().getFullYear();
@@ -44,3 +47,10 @@ licenseData = licenseData
   .replace('{{author}}', authorName);
 
 fs.writeFileSync(licensePath, licenseData, 'utf8');
+
+// Clean up
+fs.rmSync(__filename);
+
+if (!fs.readdirSync(__dirname).length) {
+  fs.rmdirSync(__dirname);
+}
